483f336aee63f392edfa6ee53abf78b4daa6adbc
sanish <s.karki1994@gmail.com>
2024-01-25 20:43:00 +0545

    Project 1- Word Scrambler using Modern Es6

--- Project1-wordScramble/script.js
+++ Project1-wordScramble/script.js
@@ -10,7 +10,7 @@ let randomWords = '';
 
 let sWords = [
   'python',
-  'javscript',
+  'javascript',
   'c++',
   'php',
   'java',
@@ -45,11 +45,17 @@ const createNewWords = () => {
 const scrambleWords = (arr) => {
   for (let i = arr.length - 1; i > 0; i--) {
     let temp = arr[i];
-    console.log(temp);
-    let j = Math.floor(Math.random() * (i + 1));
-    console.log(i);
-    console.log(j);
+    // console.log(temp);
+    let j = Math.floor(Math.random() * (i + 1)); //' i ' ma chai ordered way ma number aauxa bhane, letter lai shuffle garna lai chai ' j ' use garkeo ho,jasle random number generate garxa ra tei random number le represent garne letter lai show garxan , jas karan word shuffle bhayeko jasto dekhinxa
+    // console.log(i);
+    // console.log(j);
+
+    // aba tyo i ko thau ma j le replace garna chai Exercise-1 ma two number swap gare jastai swap garne ho
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
+
+  return arr;
 };
 
 //------2] button click bhaye paxi game suru hunu paryo
@@ -63,5 +69,29 @@ btn.addEventListener('click', function () {
     newWords = createNewWords();
 
     randomWords = scrambleWords(newWords.split('')); //split() changes strings to array / Example OP= ['r','e','a','c','t']
+
+    // console.log(randomWords.join('')); //join('') le chai array content lai combined way ma show garxa. Ex: ['a','b','c','d'] => abcd
+    msg.innerHTML = `Guess the word: ${randomWords.join('')}`;
+  } else {
+    let tempWords = guess.value; //user le input field ma type gareko word
+
+    if (newWords === tempWords) {
+      // console.log('correct');
+      play = false;
+      msg.innerHTML = `Awesome it's correct. It is ${newWords}`;
+
+      btn.innerHTML = 'Start again';
+
+      guess.classList.toggle('hidden');
+
+      guess.value = ''; // Clear input field after submit
+    } else {
+      // console.log('Incorrect');
+
+      msg.innerHTML = `Sorry it's incorrect. Please try again "${randomWords.join(
+        ''
+      )} "`;
+      guess.value = '';
+    }
   }
 });
